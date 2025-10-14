51f8541629c36d6c40cb6e3d3b49c5a34b36830e
SwarnimDev <swarnimdhawane1@gmail.com>
2025-10-14 21:09:01 +0530

    Application connect with MongoDb

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
 
+dotenv.config();
 const app = express();
+app.use(express.json());
+
+// Connect MongoDB
+connectDB();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("MongoDB + Node + TypeScript setup working!");
+});
+
 const port = process.env.PORT || 3000;
-
-app.get("/", (req:Request, res:Response)=>{
-    res.send("Hello World");
-})
-
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
